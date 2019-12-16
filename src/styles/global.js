687026a68c6dde21b39340bb4705dfcc1c3ecf9b
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-12-16 17:39:49 -0300

    Estilização da Home
    Signed-off-by: Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>

--- src/styles/global.js
+++ src/styles/global.js
@@ -6,7 +6,7 @@ export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
 
   *{
-    margin: 0px;
+    margin: 0;
     padding: 0;
     outline: 0;
     box-sizing: border-box;
