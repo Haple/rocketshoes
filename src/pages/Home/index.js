fafd46caf4a8b67f8233ac87038df5c5249c33b6
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-12-16 23:21:55 -0300

    Refatorando as actions
    Signed-off-by: Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
+import { bindActionCreators } from 'redux';
 
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 import { ProductList } from './styles';
+import * as CartActions from '../../store/modules/cart/actions';
 
 class Home extends Component {
   state = {
@@ -21,11 +23,8 @@ class Home extends Component {
   }
 
   handleAddProduct = product => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'ADD_TO_CART',
-      product,
-    });
+    const { addToCart } = this.props;
+    addToCart(product);
   };
 
   render() {
@@ -53,4 +52,7 @@ class Home extends Component {
   }
 }
 
-export default connect()(Home);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(null, mapDispatchToProps)(Home);
